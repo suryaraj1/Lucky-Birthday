59fde999c2ec9f65585e3e3ee3652f52c89a9da8
Suryaraj Bhaduri <suryaraj_bhaduri@yahoo.com>
2021-09-11 10:08:38 +0530

    Add svg for alert UI

--- src/components/AlertTile/AlertTile.js
+++ src/components/AlertTile/AlertTile.js
@@ -7,14 +7,18 @@ const AlertTile = ({ success }) => {
             {success ? (
             <div className='alert-tile'>
                 <div className='alert-tile-img-wrapper'>
-                    <img src="/image/tick-img.png" alt="alert-img"/>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
                 </div>
                 <p>Congratulations!!🎉🎉 You have a lucky birthday!</p>
             </div>
             ) : (
             <div className='alert-tile failure'>
                 <div className='alert-tile-img-wrapper failure'>
-                    <img src="/image/cross-img.svg" alt="alert-img"/>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
                 </div>
                 <p>Harsh Luck!😭 You don't have a lucky birthday..</p>
             </div>
